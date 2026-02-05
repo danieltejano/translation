a790fb9eec2dba63c60bd6b409a656cbec91eff7
Daniel Tejano <53635919+danieltejano@users.noreply.github.com>
2026-02-05 18:04:52 +0800

    Changed Factory Seeder to handle large dataset Import

--- database/seeders/TranslationSeeder.php
+++ database/seeders/TranslationSeeder.php
@@ -6,6 +6,7 @@ use App\TranslationParserTrait;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Translation;
+use Illuminate\Support\Facades\File;
 
 class TranslationSeeder extends Seeder
 {
@@ -17,11 +18,27 @@ class TranslationSeeder extends Seeder
      */
     public function run(): void
     {
-        $json_contents = json_decode(file_get_contents(base_path('/database/seeders/sample data/en_US.json')),true);
-        $translations = $this->parseFile($json_contents, 'web' ,'en');
+        $import_folder = database_path('seeders/sample data');
 
-        foreach($translations as $translation){
-            Translation::create($translation);
+        if (File::isDirectory($import_folder)) {
+            $localization_files = File::files($import_folder);
+
+            foreach ($localization_files as $localization) {
+                $locale = explode('.', $localization->getFilename())[0];
+                $locale_contents = $localization->getContents();
+
+                $this->command->info("\tImporting $locale");
+
+                $json_contents = json_decode(
+                    $locale_contents,
+                    true
+                );
+                $translations = $this->parseFile($json_contents, 'web', $locale);
+
+                // dd($translations);
+
+                Translation::insert($translations);
+            }
         }
     }
 }
