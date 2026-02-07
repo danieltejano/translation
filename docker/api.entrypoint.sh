cecf3ec4c064d23fab0c73cf6913b76de0ad0822
Daniel Tejano <53635919+danieltejano@users.noreply.github.com>
2026-02-07 09:41:02 +0800

    Docker Fixes

--- docker/api.entrypoint.sh
+++ docker/api.entrypoint.sh
@@ -6,12 +6,23 @@ while ! nc -z $DB_HOST 5432; do
     sleep 0.1
 done
 
+mkdir -p /var/www/html/storage/logs
+mkdir -p /var/www/html/storage/framework/sessions
+mkdir -p /var/www/html/storage/framework/views
+mkdir -p /var/www/html/storage/framework/cache
+mkdir -p /var/www/html/bootstrap/cache
 
-echo "Connected to $DB_CONNECTION Database"
+# Change ownership to the web user
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
+
+echo "Connected to $DB_CONNECTION Database with $DB_USERNAME $DB_PASSWORD"
 
 echo "Container is Running Migrations..."
 
-php artisan optimize:clear && php artisan optimize php artisan migrate && php artisan db:seed
+php artisan migrate --seed --force
+php artisan optimize:clear
+php artisan optimize 
 
 echo "Starting Php-FPM..."
 
